63fa64b6b17a4f2b4ccbfa696aa6e2f98619e015
toanhtran <toanh_tran@yahoo.com>
2015-09-24 22:20:10 +0900

    checkbox whipped cream

--- app/src/main/java/com/example/toanhtran/justjava2/MainActivity.java
+++ app/src/main/java/com/example/toanhtran/justjava2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.toanhtran.justjava2;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,7 +10,7 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     int quantity = 0;
 
     @Override
